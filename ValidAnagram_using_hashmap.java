48ebac7f95298ef0cd1988490a19ffd4ac514d05
aarohi3105 <aarohi3101jain@gmail.com>
2026-05-06 19:53:49 +0530

    added anagram using 2 hashmaps

---
+++ ValidAnagram_using_hashmap.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class ValidAnagram_using_hashmap {
+    public static boolean Anagram(String s , String t){
+        HashMap<Character , Integer> map1 = new HashMap<>();
+        HashMap<Character , Integer> map2 = new HashMap<>();
+        for(int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        }
+        for(int i=0;i<t.length();i++) {
+            char c = t.charAt(i);
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
+        }
+         return map1.equals(map2);
+        }
+        public static void main(String args[]){
+        String s ="anagram";
+        String t= "nagaram";
+        boolean ans = Anagram(s,t);
+        System.out.println(ans);
+        }
+    }
+
