ee50adfb230b222e15d6c2725cd255a7db75b2cf
aarohi3105 <aarohi3101jain@gmail.com>
2026-06-07 21:11:33 +0530

    linkedlist started

---
+++ LinkedList.java
@@ -0,0 +1,23 @@
+//Node creation in linkedlist
+
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+
+    public  Node head;
+    public  Node tail;
+
+    public static void main(String args[]) {
+        LinkedList ll = new LinkedList();
+        LinkedList ll1 = new LinkedList();
+
+    }
+}
