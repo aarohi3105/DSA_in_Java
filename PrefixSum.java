9c0b20acbd8e97caf00cd0a9d489d5d07950c475
aarohi3105 <aarohi3101jain@gmail.com>
2026-06-20 20:43:55 +0530

    prefix sum eg

---
+++ PrefixSum.java
@@ -0,0 +1,15 @@
+public class PrefixSum {
+
+    public static void main(String[] args){
+        int[] arr={3,1,5,2,4};
+        int n=arr.length;
+        int [] prefix = new int[n];
+        prefix[0]=arr[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+        }
+        for(int num:prefix){
+            System.out.print(num+" ");
+        }
+    }
+}
