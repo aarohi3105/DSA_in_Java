3d5741ec1e713ec143c3dd4966cb0aa60680bf91
aarohi3105 <aarohi3101jain@gmail.com>
2026-06-24 22:08:53 +0530

    program to find whether two numbers are same or different

---
+++ BitManipulation2.java
@@ -0,0 +1,11 @@
+public class BitManipulation2 {
+    public static void main(String[] args) {
+
+    //xor with same number gives 0
+
+    int no = 5;
+    System.out.println(no^no);
+
+
+}
+}
---
+++ SamenoOrNot.java
@@ -0,0 +1,14 @@
+//wirte a program to find whether two numbers are same or different
+public class SamenoOrNot {
+    public static void main(String [] args) {
+        int a = 5;
+        int b = 3;
+        if ((a^b)== 0) {
+            System.out.println("no. are same");
+        }
+        else{
+            System.out.println("no. are not same");
+        }
+    }
+
+}
